ffdebb40431f0a95c1cf8966a1cdac44d87c5bf7
Javier Alcoba <xavy1007@gmail.com>
2023-02-23 14:39:43 -0400

    hashmap probe

---
+++ src/Mapa.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Mapa {
+    public static void main(String[] args) {
+        HashMap<Integer, String> lista= new HashMap<>();
+        lista.put(1,"Algo");
+        lista.put(2,"Otroalgos");
+        System.out.println("lista = " + lista);
+    }
+}
